6f9521738b1051cdc9804d74820c7c42f37bb3c8
pablitoodt <pablodeteba@icloud.com>
2024-03-28 16:46:01 +0100

    dbTry

--- src/controllers/User.php
+++ src/controllers/User.php
@@ -2,54 +2,62 @@
 
 namespace App\Controllers;
 
+use App\Models\Usermodel;
+
 class User {
-    protected array $params;
-    protected string $reqMethod;
+  protected array $params;
+  protected string $reqMethod;
+  protected object $model;
 
-    public function __construct($params) {
-        $this->params = $params;
-        $this->reqMethod = strtolower($_SERVER['REQUEST_METHOD']);
+  public function __construct($params) {
+    $this->params = $params;
+    $this->reqMethod = strtolower($_SERVER['REQUEST_METHOD']);
+    $this->model = new Usermodel();
 
-        $this->run();
+    $this->run();
+  }
+
+  public function postUser() {
+    $body = (array) json_decode(file_get_contents('php://input'));
+
+    $this->model->add($body);
+
+    return $this->model->getLast();
+  }
+
+  public function deleteUser() {
+    return $this->model->delete(intval($this->params['id']));
+  }
+
+  public function getUser() {
+    return $this->model->get(intval($this->params['id']));
+  }
+
+  protected function header() {
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json; charset=utf-8');
+  }
+
+  protected function ifMethodExist() {
+    $method = $this->reqMethod.'User';
+
+    if (method_exists($this, $method)) {
+      echo json_encode($this->$method());
+
+      return;
     }
 
-    protected function getUser() {
-        return [
-            'firstName' => 'Cyril',
-            'lastName' => 'Vimard',
-            'promo' => 'B1',
-            'school' => 'Coda'
-        ];
-    }
+    header('HTTP/1.0 404 Not Found');
+    echo json_encode([
+      'code' => '404',
+      'message' => 'Not Found'
+    ]);
 
-    protected function header() {
-        header('Access-Control-Allow-Origin: *');
-        header('Content-type: application/json; charset=utf-8');
-    }
+    return;
+  }
 
-
-    protected function ifMethodExist() {
-        $method = $this->reqMethod.'User';
-
-        if (method_exists($this, $method)) {
-            echo json_encode($this->$method());
-
-            return;
-        }
-
-        echo json_encode([
-            'code' => '404',
-            'message' => 'Not Found'
-        ]);
-
-        return;
-    }
-
-    protected function run() {
-        $this->header();
-        $this->ifMethodExist();
-    }
+  protected function run() {
+    $this->header();
+    $this->ifMethodExist();
+  }
 }
-
-
-
