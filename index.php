05cc07109397244383a4ca4b3a6380a71fc97308
pablitoodt <pablodeteba@icloud.com>
2024-04-08 14:20:19 +0200

    remove one route

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ require 'vendor/autoload.php';
 
 
 use App\Router;
-use App\Controllers\Message;
 use App\Controllers\User;
 use App\Controllers\Messages;
 
 new Router([
   'user/:id' => User::class,
-  'message/:id' => Message::class,
   'messages' => Messages::class,
 ]);
 
